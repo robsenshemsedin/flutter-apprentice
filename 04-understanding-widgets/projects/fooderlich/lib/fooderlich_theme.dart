4d9e182ec6978a4da0b3b6e3b78ee59325e76e7a
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-12 08:54:28 +0300

    Create fooderlich_theme.dart

---
+++ 04-understanding-widgets/projects/fooderlich/lib/fooderlich_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FooderlichTheme {
+  static final lightTextTheme = TextTheme(
+    titleLarge: GoogleFonts.openSans(color: Colors.black),
+    titleMedium: GoogleFonts.openSans(color: Colors.black),
+    bodyMedium: GoogleFonts.openSans(
+        fontSize: 14, fontWeight: FontWeight.w700, color: Colors.black),
+    headlineLarge: GoogleFonts.openSans(
+        fontSize: 32, fontWeight: FontWeight.bold, color: Colors.black),
+    headlineMedium: GoogleFonts.openSans(
+        fontSize: 21, fontWeight: FontWeight.w700, color: Colors.black),
+    headlineSmall: GoogleFonts.openSans(
+        fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black),
+  );
+}
