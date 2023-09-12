33e20cb578ffb55d4e0a04fbc29508daeff73c8d
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-12 10:16:36 +0300

    Create cardImage with image path.

--- 04-understanding-widgets/projects/fooderlich/lib/card2.dart
+++ 04-understanding-widgets/projects/fooderlich/lib/card2.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({key}) : super(key: key);
+  static const cardImage = 'assets/images/mag5.png';
   @override
   Widget build(context) {
     return Center(
       child: Container(
         decoration: const BoxDecoration(
-            image: DecorationImage(image: AssetImage(''), fit: BoxFit.contain)),
+            image: DecorationImage(
+                image: AssetImage(cardImage), fit: BoxFit.contain)),
         constraints: const BoxConstraints.expand(height: 450, width: 350),
         child: const Column(),
       ),
