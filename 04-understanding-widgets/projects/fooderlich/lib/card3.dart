de568cc398cf2868fe701bb3c91d7fc86fe9b8c4
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-12 14:16:11 +0300

    Add Chips

--- 04-understanding-widgets/projects/fooderlich/lib/card3.dart
+++ 04-understanding-widgets/projects/fooderlich/lib/card3.dart
@@ -11,22 +11,80 @@ class Card3 extends StatelessWidget {
           width: 350,
           height: 450,
         ),
-        decoration: BoxDecoration(
-          color: Colors.black.withOpacity(0.9),
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/mag2.png'),
             fit: BoxFit.cover,
           ),
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderRadius: BorderRadius.all(Radius.circular(7.0)),
         ),
         child: Stack(
           children: [
             Container(
+                decoration: BoxDecoration(
               color: Colors.black.withOpacity(0.6),
+              borderRadius: const BorderRadius.all(Radius.circular(7.0)),
+            )),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.book,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+
+                  const SizedBox(height: 8),
+                  // 8
+                  Text(
+                    'Recipe Trends',
+                    style: FooderlichTheme.darkTextTheme.titleLarge,
+                  ),
+                  // 9
+                  const SizedBox(height: 30),
+                  Wrap(
+                    alignment: WrapAlignment.start,
+                    spacing: 12,
+                    children: [
+                      Chip(
+                          onDeleted: () {},
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                          label: Text(
+                            'Healthy',
+                            style: FooderlichTheme.lightTextTheme.titleSmall,
+                          )),
+                      Chip(
+                          onDeleted: () {},
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                          label: Text(
+                            'Vegan',
+                            style: FooderlichTheme.lightTextTheme.titleSmall,
+                          )),
+                      chip('Carrots'),
+                      chip('Greens'),
+                      chip('Wheat'),
+                      chip('Pescetarian'),
+                      chip('Mint'),
+                      chip('Lemongrass'),
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
       ),
     );
+  }
+
+  Widget chip(String text) {
+    return Chip(
+        backgroundColor: Colors.black.withOpacity(0.7),
+        label: Text(
+          text,
+          style: FooderlichTheme.lightTextTheme.titleSmall,
+        ));
   }
 }
