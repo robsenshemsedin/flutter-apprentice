53a8332a7d8153a6dfa6aa6f55d59fe401bfe985
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-12 08:56:26 +0300

    Created static darkTextTheme.

--- 04-understanding-widgets/projects/fooderlich/lib/fooderlich_theme.dart
+++ 04-understanding-widgets/projects/fooderlich/lib/fooderlich_theme.dart
@@ -14,4 +14,16 @@ class FooderlichTheme {
     headlineSmall: GoogleFonts.openSans(
         fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black),
   );
+  static final darkTextTheme = TextTheme(
+    titleLarge: GoogleFonts.openSans(color: Colors.white),
+    titleMedium: GoogleFonts.openSans(color: Colors.white),
+    bodyMedium: GoogleFonts.openSans(
+        fontSize: 14, fontWeight: FontWeight.w700, color: Colors.white),
+    headlineLarge: GoogleFonts.openSans(
+        fontSize: 32, fontWeight: FontWeight.bold, color: Colors.white),
+    headlineMedium: GoogleFonts.openSans(
+        fontSize: 21, fontWeight: FontWeight.w700, color: Colors.white),
+    headlineSmall: GoogleFonts.openSans(
+        fontSize: 16, fontWeight: FontWeight.w600, color: Colors.white),
+  );
 }
