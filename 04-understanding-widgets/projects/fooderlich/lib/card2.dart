b7783aefae44e4d659d705a078428bc52cfa88e6
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-12 12:04:33 +0300

    Added to Text widgets.

--- 04-understanding-widgets/projects/fooderlich/lib/card2.dart
+++ 04-understanding-widgets/projects/fooderlich/lib/card2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
 import 'author_card.dart';
 
 class Card2 extends StatelessWidget {
@@ -13,12 +14,39 @@ class Card2 extends StatelessWidget {
                 image: AssetImage(cardImage), fit: BoxFit.cover),
             borderRadius: BorderRadius.all(Radius.circular(7))),
         constraints: const BoxConstraints.expand(height: 450, width: 350),
-        child: const Column(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             AuthorCard(
               authorName: 'Mike Katz',
               title: 'Smoothie Connoisseur',
               imageProvider: AssetImage('assets/images/author_katz.jpeg'),
+            ),
+            Expanded(
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headlineLarge,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headlineLarge,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
